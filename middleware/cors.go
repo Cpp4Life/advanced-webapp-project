36de791d8e9b62ca19b3e212150c966611930505
Dat Truong <dattruong@Dats-MacBook-Pro.local>
2023-01-02 20:45:19 +0700

    cors allow all origins

--- middleware/cors.go
+++ middleware/cors.go
@@ -2,13 +2,15 @@ package middleware
 
 import (
 	"github.com/gin-contrib/cors"
+	"time"
 )
 
 func InitCors() cors.Config {
 	return cors.Config{
-		AllowOrigins:     []string{"http://localhost:7777", "http://localhost:3000"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
 		AllowHeaders:     []string{"Authorization", "Content-Type", "Content-Length", "Origin"},
 		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
 	}
 }
